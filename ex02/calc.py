633e8cf6e6676ab66d4ee7d3650d52045715540e
miyakawa <c0a21117a2@edu.teu.ac.jp>
2022-11-29 14:51:27 +0900

    練習7

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,7 +5,10 @@ def button_click(event):
     btn = event.widget
     num = btn["text"]
     if num == "=":
-        pass
+        siki = entry.get() #数式の取得
+        res = eval(siki) #式の評価
+        entry.delete(0,tk.END) #表示されている文字列の削除
+        entry.insert(tk.END, res) #結果の挿入
     else:
     #tkm.showinfo("", f"{num}ボタンがクリックされました")
         entry.insert(tk.END,num)
