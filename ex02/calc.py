5a7e4393ada5336ffbb0f4269413b71ff70c44c0
miyakawa <c0a21117a2@edu.teu.ac.jp>
2022-11-29 14:45:36 +0900

    練習6

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,7 +4,11 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn = event.widget
     num = btn["text"]
-    tkm.showinfo("", f"{num}ボタンがクリックされました")
+    if num == "=":
+        pass
+    else:
+    #tkm.showinfo("", f"{num}ボタンがクリックされました")
+        entry.insert(tk.END,num)
 
 root = tk.Tk()
 root.geometry("300x500")
@@ -24,6 +28,7 @@ operators = ["+", "="]
 for ope in operators:
     button = tk.Button(root, text = f"{ope}",width = 4, height=2, font =("", 30))
     button.grid(row = r, column= c)
+    button.bind("<1>", button_click)
     c += 1
     if c%3 == 0:
         r += 1
