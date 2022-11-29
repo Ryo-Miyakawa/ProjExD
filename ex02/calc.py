1a336f8587fccfdbc819194edd35444d19d1adcd
miyakawa <c0a21117a2@edu.teu.ac.jp>
2022-11-29 16:04:27 +0900

    四則演算、クリアボタン追加、四則演算配置変更、不成立式エラー文表示

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,19 +6,24 @@ def button_click(event):
     num = btn["text"]
     if num == "=":
         siki = entry.get() #数式の取得
-        res = eval(siki) #式の評価
+        try:
+            res = eval(siki) #式の評価
+            entry.delete(0,tk.END) #表示されている文字列の削除
+            entry.insert(tk.END, res) #結果の挿入
+        except:
+            tkm.showerror("警告", "無効な式です") #追加4：式が成り立たない場合にエラー文を出す
+    elif num == "C":
         entry.delete(0,tk.END) #表示されている文字列の削除
-        entry.insert(tk.END, res) #結果の挿入
     else:
     #tkm.showinfo("", f"{num}ボタンがクリックされました")
         entry.insert(tk.END,num)
 
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("400x600")
 
 entry = tk.Entry(root, justify = "right", width = 10, font = ("",40))
 entry.grid(row = 0, column = 0,columnspan=3)
-r,c = 1,2
+r,c = 2,2
 for i in range(9,-1, -1):
     button = tk.Button(root, text = f"{i}",width = 4, height=2, font =("", 30))
     button.grid(row = r, column= c)
@@ -27,14 +32,13 @@ for i in range(9,-1, -1):
     if c == -1:
         r += 1
         c = 2 #追加1：数の順序の変更
-operators = ["+", "="]
+operators = ["C","*", "/","+", "="] #追加2：クリアボタン、四則演算
+r = 1
+c = 3
 for ope in operators:
     button = tk.Button(root, text = f"{ope}",width = 4, height=2, font =("", 30))
     button.grid(row = r, column= c)
-    button.bind("<1>", button_click)
-    c += 1
-    if c%3 == 0:
-        r += 1
-        c = 0
+    button.bind("<1>", button_click) #追加3：四則演算の配置の変更
+    r += 1
 
 root.mainloop()
