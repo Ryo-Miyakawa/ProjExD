dbd8ccf02392f270f0bf699e47cdb862b0f88bb7
miyakawa <c0a21117a2@edu.teu.ac.jp>
2022-11-29 14:34:18 +0900

    練習4

--- ex02/calc.py
+++ ex02/calc.py
@@ -8,7 +8,10 @@ def button_click(event):
 
 root = tk.Tk()
 root.geometry("300x500")
-r,c = 0,0
+
+entry = tk.Entry(root, justify = "right", width = 10, font = ("",40))
+entry.grid(row = 0, column = 0,columnspan=3)
+r,c = 1,0
 for i in range(9,-1, -1):
     button = tk.Button(root, text = f"{i}",width = 4, height=2, font =("", 30))
     button.grid(row = r, column= c)
