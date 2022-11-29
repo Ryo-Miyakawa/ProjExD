2de686e95cea8168449301fda006c88fb20ddde7
miyakawa <c0a21117a2@edu.teu.ac.jp>
2022-11-29 15:29:30 +0900

    数順序変更

--- ex02/calc.py
+++ ex02/calc.py
@@ -18,15 +18,15 @@ root.geometry("300x500")
 
 entry = tk.Entry(root, justify = "right", width = 10, font = ("",40))
 entry.grid(row = 0, column = 0,columnspan=3)
-r,c = 1,0
+r,c = 1,2
 for i in range(9,-1, -1):
     button = tk.Button(root, text = f"{i}",width = 4, height=2, font =("", 30))
     button.grid(row = r, column= c)
     button.bind("<1>", button_click)
-    c += 1
-    if c%3 == 0:
+    c -= 1
+    if c == -1:
         r += 1
-        c = 0
+        c = 2 #追加1：数の順序の変更
 operators = ["+", "="]
 for ope in operators:
     button = tk.Button(root, text = f"{ope}",width = 4, height=2, font =("", 30))
