876de1fd5a4ac1a819b45feb5dcc9e85330d0ce6
miyakawa <c0a21117a2@edu.teu.ac.jp>
2022-12-06 15:42:07 +0900

    練習3-12修正

---
+++ ex03-2/maze.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+import maze_maker as mm
+
+def key_down(event):
+    global key
+    key = event.keysym
+
+def key_up(event):
+    global key
+    key = ""
+
+def main_proc():
+    global cx,cy,mx,my
+    if key == "Up":my -= 1
+    if key == "Down":my += 1
+    if key == "Left":mx -=1
+    if key == "Right":mx += 1
+    if maze_lst[mx][my] == 1: #移動先が壁だったら
+        if key == "Up":my += 1
+        if key == "Down":my -= 1
+        if key == "Left":mx +=1
+        if key == "Right":mx -= 1
+    cx,cy = mx*100+50, my*100+50
+    canvas.coords("kokaton",cx,cy)
+    root.after(100,main_proc)
+
+
+if __name__ == "__main__":
+    root= tk.Tk()
+    root.title("迷えるこうかとん")
+    canvas = tk.Canvas(root, width= 1500, height = 900, bg = "black")
+    canvas.pack()
+
+    maze_lst = mm.make_maze(15, 9)
+    #print(maze_lst)
+    mm.show_maze(canvas, maze_lst)
+
+    mx,my = 1,1
+    cx,cy = mx*100+50, my*100+50
+    kokaton = tk.PhotoImage(file = "fig/8.png")
+    
+
+    canvas.create_image(cx, cy, 
+                        image = kokaton,
+                        tag = "kokaton")
+
+
+    key =""
+    root.bind("<KeyPress>",key_down)
+    root.bind("<KeyRelease>", key_up)
+    main_proc()
+    root.mainloop()
