eb2c3f8a9188ea08856cc4505d84787c6f698371
miyakawa <c0a21117a2@edu.teu.ac.jp>
2022-12-13 17:15:01 +0900

    追加機能実装、readme追加

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -9,9 +9,14 @@ def check_bound(obj_rct,scr_rct):
     yoko, tate = +1, +1
     if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
         yoko = -1
+
     if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
         tate = -1
     return yoko, tate
+
+
+
+
 
 
 def main():
@@ -40,6 +45,10 @@ def main():
     bomb_rct.centery = random.randint(0,scrn_rct.height)
     scrn_sfc.blit(bomb_sfc,bomb_rct)
 
+
+
+
+
     vx, vy = +1, +1
     #練習2
     while True:
@@ -57,6 +66,11 @@ def main():
             tori_rct.centerx -= 1
         if key_dct[pg.K_RIGHT]:
             tori_rct.centerx += 1
+        if key_dct[pg.K_ESCAPE]:
+            vx,vy = 0,0 #追加機能２:エスケープを押すと弾が停止
+        if key_dct[pg.K_1]:
+            vx, vy = +1,+1 #追加機能３　１を押すと弾が方向を変える
+            
         if check_bound(tori_rct,scrn_rct) != (+1,+1):
             if key_dct[pg.K_UP]:
                 tori_rct.centery += 1
@@ -66,18 +80,40 @@ def main():
                 tori_rct.centerx += 1
             if key_dct[pg.K_RIGHT]:
                 tori_rct.centerx -= 1
+            
+
+            
+                
+
         scrn_sfc.blit(tori_sfc, tori_rct)
         
         #練習6
+        if key_dct[pg.K_2]:#追加機能4弾の大きさを大きくする
+            byoko = bomb_rct.centerx
+            btate = bomb_rct.centery
+            bomb_sfc = pg.Surface((100,100))
+            bomb_sfc.set_colorkey((0,0,0))
+            pg.draw.circle(bomb_sfc,(255,0,0),(50,50),50)
+            bomb_rct = bomb_sfc.get_rect()
+            bomb_rct.centerx = byoko
+            bomb_rct.centery = btate
+            scrn_sfc.blit(bomb_sfc,bomb_rct)
         bomb_rct.move_ip(vx, vy)
         scrn_sfc.blit(bomb_sfc,bomb_rct)
         yoko,tate = check_bound(bomb_rct,scrn_rct)
         vx *= yoko
         vy *= tate
-
+        
         #練習8
         if tori_rct.colliderect(bomb_rct):
-            return
+            reyoko = tori_rct.centerx
+            retate = tori_rct.centery
+            tori_sfc = pg.image.load("fig/kfc.png")
+            tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 0.5)
+            tori_rct = tori_sfc.get_rect()
+            tori_rct.center = (reyoko,retate)
+            scrn_sfc.blit(tori_sfc, tori_rct)#追加機能１：爆弾がこうかとんに当たると画像変更
+           
         
         pg.display.update()
         clock.tick(1000)
